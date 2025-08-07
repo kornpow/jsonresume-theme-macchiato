3b1a3c1b4350adab4cca8c3a4734ebcccbfddf82
Sam Korn <korn94sam@gmail.com>
2025-08-07 10:06:20 -0600

    feat: more formatting and tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   purge: false,
   content: ['./src/**/*.hbs', './src/**/*.js', './src/**/*.css', './resume.json'],
-  theme: { 
+  theme: {
     extend: {
       colors: {
         'brand-teal': '#56817A',
-      }
-    } 
+      },
+      fontFamily: {
+        sans: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
+        heading: ['Josefin Sans', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
